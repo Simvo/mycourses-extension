31591c5e91a6a4746af47061b31345a67cd5f09c
demetrios-koziris <dwkoziris@gmail.com>
2018-02-06 12:06:37 -0500

    initial working version of feature adding calendar shortcut to navigation bar

--- src/js/calendarShortcut.js
+++ src/js/calendarShortcut.js
@@ -1,12 +1,25 @@
+//jshint esversion: 6
 
 function calendarShortcut() {
 
-	devlog("Executing calendarShortcut.js");	
+	devlog("Executing calendarShortcut.js");
 
+	let calendarURL = '/d2l/le/calendar/6606';
+	if (!url.pathname.match(/\/d2l\/home$/)) {
+		calendarURL = document.getElementsByClassName('d2l-homepage-heading-link')[1].href;
+	}
 
+	let calendarLinkDiv = document.createElement('div');
+	calendarLinkDiv.className = 'd2l-navigation-s-item';
+	calendarLinkDiv.setAttribute('role', 'listitem');
 
+	let calendarLink = document.createElement('a');
+	calendarLink.className = 'd2l-navigation-s-link';
+	calendarLink.href = calendarURL;
+	calendarLink.innerText = 'Calendar';
+	calendarLinkDiv.appendChild(calendarLink);
 
-
-
+	let navigationWrapper = document.getElementsByClassName('d2l-navigation-s-main-wrapper')[0];
+	navigationWrapper.insertBefore(calendarLinkDiv, navigationWrapper.firstChild);
 
 }
