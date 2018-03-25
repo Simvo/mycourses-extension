94600e219b343af2bd58436a11836fb12eebcf65
Lucy <sam.lucy.xiang@gmail.com>
2018-03-24 23:07:48 -0400

    added options page and option for calender toggle

--- src/js/calendarToggle.js
+++ src/js/calendarToggle.js
@@ -1,7 +1,23 @@
-function calendarToggle(){
-    devlog("Executing calendarToggle.js");
+function calendarToggle() {
+	
+	
+		devlog("Executing calendarToggle.js");
 
-    let collapsedContents = document.getElementsByClassName("d2l-collapsepane-header");;
-    let childrens = collapsedContents[0].children[0].children[0];
-          childrens.click();
+		function getCalenderStatus(callback){
+		var calenderStatus="";
+		chrome.storage.sync.get('calender', function (obj) {
+		calenderStatus = obj.calender;
+		callback(calenderStatus);
+		});
+	}
+
+		function toggle(calenderStatus){
+			if(calenderStatus){
+		let collapsedContents = document.getElementsByClassName("d2l-collapsepane-header");;
+    	let childrens = collapsedContents[0].children[0].children[0];
+    	childrens.click();
+    }
+}
+
+getCalenderStatus(toggle);
 }
